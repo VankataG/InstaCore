e7b55ff4e7c503e0c9926a358117dca18150fdef
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-07 19:08:29 +0200

    Added UserConfiguration

---
+++ InstaCore.Infrastructure/Configuration/UserConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using InstaCore.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InstaCore.Infrastructure.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> entity)
+        {
+            entity
+                .HasKey(u => u.Id);
+
+            entity
+                .Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity
+                .Property(u => u.PasswordHash)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity
+                .Property(u => u.Bio)
+                .HasMaxLength(500);
+
+            entity
+                .Property(u => u.AvatarUrl)
+                .HasMaxLength(512);
+
+            entity
+                .Property(u => u.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
