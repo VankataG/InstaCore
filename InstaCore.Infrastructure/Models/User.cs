68ec3f16610fd60614bcf8ce9a2b97a720dd8d9d
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-07 19:53:34 +0200

    Added virtual colllection for lazy loading in User

--- InstaCore.Infrastructure/Models/User.cs
+++ InstaCore.Infrastructure/Models/User.cs
@@ -16,5 +16,17 @@
 
         public DateTime CreatedAt { get; set; }
 
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+
+        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
+
+        public virtual ICollection<Like> Likes { get; set; } = new HashSet<Like>();
+
+        public virtual ICollection<Follow> Followers { get; set; } = new HashSet<Follow>();
+
+        public virtual ICollection<Follow> Following { get; set; } = new HashSet<Follow>();
+
+
+
     }
 }
