4bb3b0688486306dca6b2af07dafba3c6457c53d
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-07 19:52:52 +0200

    Added Comment Configuration

---
+++ InstaCore.Infrastructure/Configuration/CommentConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using InstaCore.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InstaCore.Infrastructure.Configuration
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> entity)
+        {
+            entity
+                .HasKey(c => c.Id);
+
+            entity
+                .Property(c => c.Text)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity
+                .HasOne(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity
+                .Property(c => c.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
--- InstaCore.Infrastructure/Models/Comment.cs
+++ InstaCore.Infrastructure/Models/Comment.cs
@@ -6,10 +6,16 @@
 
         public Guid PostId { get; set; }
 
+        public required Post Post { get; set; }
+
         public Guid UserId { get; set; }
+
+        public required User User { get; set; }
 
         public required string Text { get; set; }
 
         public DateTime CreatedAt { get; set; }
+
+
     }
 }
