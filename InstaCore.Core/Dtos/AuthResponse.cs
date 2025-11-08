dcebf43d64bcca3fcc1e20832fa39da3d79bbb40
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-08 18:51:50 +0200

    Added Dtos

---
+++ InstaCore.Core/Dtos/AuthResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace InstaCore.Core.Dtos
+{
+    public class AuthResponse
+    {
+        public required string Token { get; set; }
+
+        public required string Username { get; set; }
+
+        public required Guid UserId { get; set; }
+    }
+}
---
+++ InstaCore.Core/Dtos/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace InstaCore.Core.Dtos
+{
+    public class RegisterRequest
+    {
+        public required string Username { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
