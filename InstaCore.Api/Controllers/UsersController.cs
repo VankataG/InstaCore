1f91a842f35bf9ccb7dcc2905b0d728d042ac7f9
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-11 19:23:44 +0200

    Add private UsersController for testing Jwt

---
+++ InstaCore.Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaCore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("me")]
+        [Authorize]
+        public IActionResult GetCurrentUser()
+        {
+            var username = User.Identity?.Name;
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return Ok(new { username, userId});
+        }
+    }
+}
