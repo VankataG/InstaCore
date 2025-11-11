31219974df3a6bff323d0301e2e52aa473d8ee41
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-11 06:47:29 +0200

    Implemented AuthService

---
+++ InstaCore.Core/Services/AuthService.cs
@@ -0,0 +1,73 @@
+﻿using InstaCore.Core.Contracts;
+using InstaCore.Core.Dtos;
+using InstaCore.Core.Exceptions;
+using InstaCore.Core.Models;
+
+namespace InstaCore.Core.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository userRepository;
+
+        private readonly IPasswordHasher passwordHasher;
+
+        private readonly IJwtTokenService jwtTokenService;
+
+        public AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher, IJwtTokenService jwtTokenService)
+        {
+            this.userRepository = userRepository;
+            this.passwordHasher = passwordHasher;
+            this.jwtTokenService = jwtTokenService;
+        }
+
+
+        public async Task<AuthResponse> LoginAsync(LoginRequest request)
+        {
+            User? user = await userRepository.GetByEmailAsync(request.Email);
+
+            if (user == null || !passwordHasher.Verify(request.Password, user.PasswordHash))
+                throw new UnauthorizedException();
+
+
+            var token = jwtTokenService.CreateAccessToken(user);
+
+            return new AuthResponse()
+            {
+                Token = token,
+                UserId = user.Id,
+                Username = user.Username,
+            };
+
+
+        }
+
+        public async Task<AuthResponse> RegisterAsync(RegisterRequest request)
+        {
+            if (await userRepository.ExistsByEmailAsync(request.Email) ||
+                await userRepository.ExistsByUsernameAsync(request.Username))
+            {
+                throw new ConflictException("Email or Username already taken.");
+            }
+
+            string hashedPassword = passwordHasher.Hash(request.Password);
+
+            User newUser = new User()
+            {
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = hashedPassword
+            };
+
+            await userRepository.AddAsync(newUser);
+
+            var token = jwtTokenService.CreateAccessToken(newUser);
+
+            return new AuthResponse()
+            { 
+                Token = token,
+                UserId = newUser.Id,
+                Username = newUser.Username,
+            };
+        }
+    }
+}
