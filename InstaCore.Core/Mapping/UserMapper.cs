edfdea3516ca81075c444fdce6bf33fbfa5b10c3
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-12 20:04:21 +0200

    Created UserMapper for DRY

---
+++ InstaCore.Core/Mapping/UserMapper.cs
@@ -0,0 +1,16 @@
+﻿using InstaCore.Core.Dtos.Users;
+using InstaCore.Core.Models;
+
+namespace InstaCore.Core.Mapping
+{
+    public static class UserMapper
+    {
+        public static UserResponse ToResponse(User user) => new()
+        {
+            Id = user.Id,
+            Username = user.Username,
+            Bio = user.Bio,
+            AvatarUrl = user.AvatarUrl
+        };
+    }
+}
