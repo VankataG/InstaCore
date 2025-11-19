de5347eb3c8020a2ccb5353f62a4fce4b1f02c23
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-19 19:48:09 +0200

    Implemented ErrorHandlingMiddleware

---
+++ InstaCore.Api/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using InstaCore.Core.Exceptions;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace InstaCore.Api.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (NotFoundException ex)
+            {
+                await WriteProblem(context, 404, "Not found", ex.Message);
+            }
+            catch (UnauthorizedException ex)
+            {
+                await WriteProblem(context, 401, "Unauthorized", ex.Message);
+            }
+            catch (ConflictException ex)
+            {
+                await WriteProblem(context, 409, "Conflict", ex.Message);
+            }
+            catch (BadRequestException ex)
+            {
+                await WriteProblem(context, 400, "Bad request", ex.Message);
+            }
+            catch (Exception)
+            {
+                await WriteProblem(context, 500, "Server error", "Something went wrong.");
+            }
+        }
+
+        public static Task WriteProblem(HttpContext context, int status, string title, string message)
+        {
+            context.Response.StatusCode = status;
+            context.Response.ContentType = "application/json";
+            var problem = new { title, status, message };
+            return context.Response.WriteAsJsonAsync(problem);
+        }
+    }
+}
