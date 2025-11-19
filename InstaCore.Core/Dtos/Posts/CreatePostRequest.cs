fa8cd9c2ea05afbbd45a2497010775c3d4113dfe
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-19 07:23:21 +0200

    Created Dtos for Post logic implementation

---
+++ InstaCore.Core/Dtos/Posts/CreatePostRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace InstaCore.Core.Dtos.Posts
+{
+    public class CreatePostRequest
+    {
+        public required string Caption { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ InstaCore.Core/Dtos/Posts/PostResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace InstaCore.Core.Dtos.Posts
+{
+    public class PostResponse
+    {
+        public Guid Id { get; set; }
+
+        public required string Username { get; set; }
+
+        public required string Caption { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+
+    }
+}
