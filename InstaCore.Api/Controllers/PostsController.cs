4066b3c3e7ceea9370c0232f1e86357ffc632338
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-19 07:49:56 +0200

    Implemented Controller Endpoints - Working

---
+++ InstaCore.Api/Controllers/PostsController.cs
@@ -0,0 +1,58 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Principal;
+using InstaCore.Core.Dtos.Posts;
+using InstaCore.Core.Exceptions;
+using InstaCore.Core.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaCore.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostService postService;
+
+        public PostsController(IPostService postService)
+        {
+            this.postService = postService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreatePostRequest request)
+        {
+            var sub = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+
+            if (!Guid.TryParse(sub, out var userId))
+                return Unauthorized();
+
+            try
+            {
+                PostResponse response = await postService.CreateAsync(userId, request);
+                return CreatedAtAction(nameof(GetById), new { id = response.Id }, response);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                PostResponse response = await postService.GetByIdAsync(id);
+                return Ok(response);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
