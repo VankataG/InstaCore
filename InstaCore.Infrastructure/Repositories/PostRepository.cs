90e61656a958a28c016b60ca3b290ef6101709b5
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-19 07:24:08 +0200

    Implemented PostRepository (Abstract it later)

---
+++ InstaCore.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,38 @@
+﻿using InstaCore.Core.Contracts.Repos;
+using InstaCore.Core.Models;
+using InstaCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstaCore.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly InstaCoreDbContext dbContext;
+
+        public PostRepository(InstaCoreDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public async Task AddAsync(Post post)
+        {
+            await dbContext.Posts.AddAsync(post);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Post?> GetByIdAsync(Guid id)
+        {
+            return await dbContext
+                .Posts
+                .Include(p => p.User)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public Task<IReadOnlyList<Post>> GetByUserAsync(Guid userId, int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
