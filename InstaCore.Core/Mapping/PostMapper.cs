1588442fea9f16108ed5111d99fb2fcbacdb20b5
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-19 07:24:33 +0200

    Created Mapper for DRY creating response

---
+++ InstaCore.Core/Mapping/PostMapper.cs
@@ -0,0 +1,17 @@
+﻿using InstaCore.Core.Dtos.Posts;
+using InstaCore.Core.Models;
+
+namespace InstaCore.Core.Mapping
+{
+    public static class PostMapper
+    {
+        public static PostResponse ToResponse(Post post, User user) => new()
+        {
+            Id = post.Id,
+            Username = user.Username,
+            Caption = post.Caption,
+            ImageUrl = post.ImageUrl,
+            CreatedAt = post.CreatedAt
+        };
+    }
+}
