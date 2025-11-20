81584cd3ac62c97b431c4f94e725fff7d824e2b4
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-20 06:54:58 +0200

    Add extensions for cleaner ErrorHandling and Swagger implementation

---
+++ InstaCore.Api/Extensions/ApiExtensions.cs
@@ -0,0 +1,53 @@
+﻿using InstaCore.Api.Middlewares;
+using Microsoft.OpenApi.Models;
+
+namespace InstaCore.Api.Extensions
+{
+    public static class ApiExtensions
+    {
+        public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
+            return app;
+        }
+
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddOpenApi();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "InstaCore API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter: {your JWT token}"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
