9c75415201a63d8f91919a0a7226baac1d3a96c8
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-22 14:09:31 +0200

    Implemented LikeService

---
+++ InstaCore.Core/Services/Contracts/ILikeService.cs
@@ -0,0 +1,11 @@
+﻿using InstaCore.Core.Dtos.Likes;
+
+namespace InstaCore.Core.Services.Contracts
+{
+    public interface ILikeService
+    {
+        public Task<LikeResponse> LikeAsync(Guid userId, Guid postId);
+
+        public Task<LikeResponse> UnlikeAsync(Guid userId, Guid postId);
+    }
+}
---
+++ InstaCore.Core/Services/LikeService.cs
@@ -0,0 +1,90 @@
+﻿using InstaCore.Core.Contracts.Repos;
+using InstaCore.Core.Dtos.Likes;
+using InstaCore.Core.Exceptions;
+using InstaCore.Core.Models;
+using InstaCore.Core.Services.Contracts;
+
+namespace InstaCore.Core.Services
+{
+    public class LikeService : ILikeService
+    {
+        private readonly IPostRepository postRepository;
+
+        private readonly IUserRepository userRepository;
+
+        private readonly ILikeRepository likeRepository;
+
+        public LikeService(IPostRepository postRepository,IUserRepository userRepository, ILikeRepository likeRepository)
+        {
+            this.postRepository = postRepository;
+            this.userRepository = userRepository;
+            this.likeRepository = likeRepository;
+        }
+
+
+        public async Task<LikeResponse> LikeAsync(Guid userId, Guid postId)
+        {
+            Post? post = await postRepository.GetByIdAsync(postId);
+
+            if(post == null) 
+                throw new NotFoundException("Post not found.");
+
+            User? user = await userRepository.GetByIdAsync(userId);
+
+            if (user == null)
+                throw new NotFoundException("User not found.");
+
+            Like? existingLike = await likeRepository.GetByUserAndPostAsync(userId, postId);
+
+            if (existingLike != null)
+                throw new ConflictException("You already liked this post.");
+
+            Like newLike = new Like()
+            { 
+                PostId = postId,
+                UserId = userId
+            };
+
+            await likeRepository.AddAsync(newLike);
+
+            return new LikeResponse()
+            {
+                LikeId = newLike.Id,
+                PostId = postId,
+                Username = user.Username,
+                Liked = true,
+                TotalLikes = post.Likes.Count,
+                CreatedAt = newLike.CreatedAt
+            };
+        }
+
+        public async Task<LikeResponse> UnlikeAsync(Guid userId, Guid postId)
+        {
+            Post? post = await postRepository.GetByIdAsync(postId);
+
+            if (post == null)
+                throw new NotFoundException("Post not found.");
+
+            User? user = await userRepository.GetByIdAsync(userId);
+
+            if (user == null)
+                throw new NotFoundException("User not found.");
+
+            Like? like = await likeRepository.GetByUserAndPostAsync(userId, postId);
+
+            if (like == null)
+                throw new ConflictException("You haven't liked this post.");
+
+
+            await likeRepository.DeleteAsync(like);
+
+            return new LikeResponse()
+            {
+                PostId = postId,
+                Username = user.Username,
+                Liked = false,
+                TotalLikes = post.Likes.Count - 1
+            };
+        }
+    }
+}
