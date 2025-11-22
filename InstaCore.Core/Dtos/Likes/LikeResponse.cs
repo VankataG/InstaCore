5430a6741f34e66efb7b6fc993f76bb52517d953
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-22 14:09:43 +0200

    Created Like Dto

---
+++ InstaCore.Core/Dtos/Likes/LikeResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace InstaCore.Core.Dtos.Likes
+{
+    public class LikeResponse
+    {
+        public Guid? LikeId { get; set; }
+
+        public Guid PostId { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public bool Liked { get; set; }
+
+        public int TotalLikes { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
