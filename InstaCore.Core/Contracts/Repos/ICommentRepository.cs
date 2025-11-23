1bbd0d2530c3bbf24fb1b118fc841e2f9a36cc80
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-23 07:27:31 +0200

    Added Comment Repository

---
+++ InstaCore.Core/Contracts/Repos/ICommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using InstaCore.Core.Models;
+
+namespace InstaCore.Core.Contracts.Repos
+{
+    public interface ICommentRepository
+    {
+        public Task<Comment?> GetByIdAsync(Guid id);
+
+        public Task AddAsync(Comment comment);
+
+        public Task DeleteAsync(Comment comment);
+
+        public Task<IEnumerable<Comment>> GetAllCommentsAsync(Guid postId);
+    }
+}
---
+++ InstaCore.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,48 @@
+﻿using InstaCore.Core.Contracts.Repos;
+using InstaCore.Core.Models;
+using InstaCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstaCore.Infrastructure.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly InstaCoreDbContext dbContext;
+
+        public CommentRepository(InstaCoreDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public async Task AddAsync(Comment comment)
+        {
+            await dbContext.Comments.AddAsync(comment);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Comment comment)
+        {
+            dbContext.Comments.Remove(comment);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllCommentsAsync(Guid postId)
+        {
+            return await dbContext
+                .Comments
+                .Include(c => c.User)
+                .AsNoTracking()
+                .Where(c => c.PostId == postId)
+                .ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(Guid id)
+        {
+            return await dbContext
+                .Comments
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
