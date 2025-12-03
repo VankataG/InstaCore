92e2c3300acf8d57c76a73adf90be7bdebbb4f18
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-03 06:44:44 +0200

    Add client.ts to simplify logic in other /api sources

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -1,5 +1,38 @@
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// export async function apiFetch<T = any>(path: string, options: RequestInit = {}): Promise<T>{
+type RequestOptions= {
+    method?: "GET" | "PUT" | "POST" | "DELETE";
+    token?: string | null;
+    body?: unknown;
+}
 
-// }
+export async function apiFetch<TResponse>(path: string, options: RequestOptions = {}): Promise<TResponse>{
+    const { method = "GET", token, body } = options
+
+    const headers: HeadersInit = {
+        "Content-Type": "application/json",
+    };
+
+    if (token){
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    const response = await fetch(`${API_BASE_URL}${path}`, { 
+        method, 
+        headers,
+        body: body !== undefined ? JSON.stringify(body) : undefined
+    });
+
+    if (!response.ok){
+        const errorBody = await response.json().catch(() => null as any);
+        const message = errorBody?.detail || errorBody?.title || `Request failed: ${response.status}`;
+
+        throw new Error(message);
+    }
+
+    if (response.status === 204){
+        return undefined as TResponse;
+    }
+
+    return await response.json() as TResponse;
+}
