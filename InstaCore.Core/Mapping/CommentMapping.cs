82f5a5be6c9c536405a30beb2fd2f4353031f201
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-07 12:17:36 +0200

    Create CommentMapping

---
+++ InstaCore.Core/Mapping/CommentMapping.cs
@@ -0,0 +1,17 @@
+﻿using InstaCore.Core.Dtos.Comments;
+using InstaCore.Core.Models;
+
+namespace InstaCore.Core.Mapping
+{
+    public static class CommentMapping
+    {
+        public static CommentResponse ToResponse(Comment comment) => new()
+        {
+            CommentId = comment.Id,
+            PostId = comment.PostId,
+            Username = comment.User.Username,
+            Text = comment.Text,
+            CreatedAt = comment.CreatedAt
+        };
+    }
+}
