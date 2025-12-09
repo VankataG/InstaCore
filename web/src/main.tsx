b8619deae654c873e45923d689bdf39b94f8079f
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-09 07:43:45 +0200

    Add in main tree

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import { UserProvider } from './contexts/UserContext.tsx';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>,
 );
