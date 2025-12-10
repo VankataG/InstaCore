b3f2fc35380ad2019653f7bf5967714bcbac0e93
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-10 17:53:16 +0200

    use NavBar component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,37 +1,22 @@
 
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MePage from "./pages/MePage/MePage";
 import PublicProfilePage from "./pages/PublicProfilePage/PublicProfilePage";
 import FeedPage from "./pages/FeedPage/FeedPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
+import NavBar from "./components/NavBar/NavBar";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div style={{ fontFamily: "sans-serif" }}>
+    <div className={styles.app}>
+      <NavBar />
       
-      <header
-        style={{
-          padding: "1rem 2rem",
-          borderBottom: "1px solid #eee",
-          marginBottom: "1rem",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <div>
-          <Link to="/" style={{ textDecoration: "none", fontWeight: 700 }}>
-            InstaCore
-          </Link>
-        </div>
-        <nav style={{ display: "flex", gap: "1rem" }}>
-          <Link to="/login">Login</Link>
-          <Link to="/me">My Profile</Link>
-          <Link to="/feed">Feed</Link>
-        </nav>
-      </header>
 
-      <main>
+      <main className={styles.main}>
         <Routes>
           <Route path="/" element={<Navigate to="/register" />} />
           <Route path="/login" element={<LoginPage />} />
@@ -39,7 +24,6 @@ function App() {
           <Route path="/u/:username" element={<PublicProfilePage />} />
           <Route path="/feed" element={<FeedPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          {/* later: feed, post details, etc. */}
         </Routes>
       </main>
     </div>
