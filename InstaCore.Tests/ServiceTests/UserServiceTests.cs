1b746a7ef8380caa40739fa8e9f7847fd1c9861d
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-11 22:25:56 +0200

    Created and configured test class for UserService

---
+++ InstaCore.Tests/ServiceTests/UserServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using InstaCore.Core.Contracts.Repos;
+using InstaCore.Core.Services;
+using InstaCore.Core.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace InstaCore.Tests.ServiceTests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        private Mock<IUserRepository> userRepositoryMock;
+        private Mock<IFollowRepository> followRepositoryMock;
+        private IUserService userService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.userRepositoryMock = new Mock<IUserRepository>();
+            this.followRepositoryMock = new Mock<IFollowRepository>();
+            this.userService = new UserService(userRepositoryMock.Object, followRepositoryMock.Object);
+        }
+
+        [Test]
+        public void PassAlways()
+        {
+            Assert.Pass();
+        }
+    }
+}
