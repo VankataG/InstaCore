2244db6ea213bd04f8816d36728792f5596fd6d8
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-13 22:17:46 +0200

    Create uploads utility for making requests to the Api

---
+++ web/src/utils/uploads.ts
@@ -0,0 +1,20 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+
+
+export async function uploadPostImage(file: File, token: string): Promise<{ url: string}>{
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await fetch(`${API_BASE_URL}/api/uploads/post-image`, {
+        method: "POST",
+        headers: {
+            'Authorization': `Bearer ${token}`
+            },
+        body: formData
+        
+    })
+
+    return response.json();
+}
