0b3578d231cd6fa008eb2935bb002e3efae87797
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-14 16:48:29 +0200

    Add UploadAvatar mehod same as for PostImage

--- InstaCore.Api/Controllers/UploadsController.cs
+++ InstaCore.Api/Controllers/UploadsController.cs
@@ -19,9 +19,44 @@ namespace InstaCore.Api.Controllers
 
 
         [HttpPost("avatar")]
-        public IActionResult UploadAvatar(IFormFile file)
+        public async Task<IActionResult> UploadAvatar(IFormFile file)
         {
-            throw new NotImplementedException();
+            if (file is null)
+                return BadRequest("File is required");
+
+            if (file.Length == 0)
+                return BadRequest("Empty file");
+
+            long maxBytes = 5 * 1024 * 1024;
+
+            if (file.Length > maxBytes)
+                return BadRequest("File is too large to upload");
+
+
+            List<string> allowedExts = new() { ".jpg", ".jpeg", ".png", ".webp" };
+
+            string? ext = Path.GetExtension(file.FileName).ToLower();
+
+            if (!allowedExts.Contains(ext))
+                return BadRequest("Extension not allowed");
+
+
+
+            string filename = Guid.NewGuid().ToString("N") + ext;
+
+            string dirPath = Path.Combine(env.WebRootPath, "uploads", "avatars");
+            Directory.CreateDirectory(dirPath);
+
+            string filePath = Path.Combine(dirPath, filename);
+
+            using (var fs = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            string url = $"/uploads/avatars/{filename}";
+
+            return Ok(new { url });
         }
 
         [HttpPost("post-image")]
