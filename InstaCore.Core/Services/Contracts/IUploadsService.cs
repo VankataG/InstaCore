7db89966a079adc5c882c11b42fdd123b423ecc9
Ivan Ganchev <djonatan2002@gmail.com>
2025-12-15 20:56:47 +0200

    Create interface for uploads service

---
+++ InstaCore.Core/Services/Contracts/IUploadsService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace InstaCore.Core.Services.Contracts
+{
+    public interface IUploadsService
+    {
+        Task<string> UploadImageAsync(IFormFile file, string rootPath, string saveFolderPath);
+    }
+}
